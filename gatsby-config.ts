d74bb5241523d577bd094ccf1bf715778f4378fe
Kwinten Fruytier <kwinten@thewave.tech>
2022-11-16 17:06:07 +0100

    trying to fix tag manager

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -93,26 +93,14 @@ const config: GatsbyConfig = {
             },
         },
         {
-            resolve: `gatsby-plugin-google-gtag`,
+            resolve: 'gatsby-plugin-google-tagmanager',
             options: {
-                // You can add multiple tracking ids and a pageview event will be fired for all of them.
-                trackingIds: ['GTM-T23N24V'],
-                // This object gets passed directly to the gtag config command
-                // This config will be shared across all trackingIds
-                gtagConfig: {
-                    optimize_id: 'OPT_CONTAINER_ID',
-                    anonymize_ip: true,
-                    cookie_expires: 0,
-                },
-                // This object is used for configuration specific to this plugin
-                pluginConfig: {
-                    // Puts tracking script in the head instead of the body
-                    head: false,
-                    // Setting this parameter is also optional
-                    respectDNT: true,
-                    // Avoids sending pageview hits from custom paths
-                    exclude: ['/preview/**', '/do-not-track/me/too/'],
-                },
+                id: 'GTM-T23N24V',
+
+                // Include GTM in development.
+                //
+                // Defaults to false meaning GTM will only be loaded in production.
+                includeInDevelopment: false,
             },
         },
         {
