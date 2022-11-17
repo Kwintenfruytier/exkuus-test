42b9658e7c58412dbe70805bba540de02c6f250b
Kwinten Fruytier <kwinten@thewave.tech>
2022-11-17 21:53:28 +0100

    added back to html

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -3,7 +3,7 @@ import { cloneElement, React } from 'react';
 import { LayoutProvider } from 'layouts/LayoutProvider';
 import { DefaultLayout } from 'layouts/DefaultLayout';
 import type { GatsbySSR } from 'gatsby';
-import { Script, ScriptStrategy } from 'gatsby';
+import { Script } from 'gatsby';
 import '@fontsource/cairo';
 import '@fontsource/text-me-one';
 
@@ -23,31 +23,4 @@ export const wrapPageElement: GatsbySSR['wrapPageElement'] = ({ element, props }
             <Script src="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;500;600;700;800;900;1000&display=swap" />
         </Layout>
     );
-};
-export const onRenderBody = ({ setHeadComponents }) => {
-    setHeadComponents([
-        <script
-            key="partytown-vanilla-config"
-            dangerouslySetInnerHTML={{
-                __html: `partytown = {
-           resolveUrl(url, location) {
-              if (url.hostname.includes('google-analytics')) {
-                // Use a secure connection
-                if (url?.protocol === 'http:') {
-                  url = new URL(url.href.replace('http', 'https'))
-                }
-
-                // Point to our proxied URL
-                const proxyUrl = new URL(location.origin + '/__third-party-proxy')
-                proxyUrl.searchParams.append('url', url)
-
-                return proxyUrl
-              }
-
-              return url
-           }
-         }`,
-            }}
-        />,
-    ]);
 };
--- src/html.js
+++ src/html.js
@@ -12,26 +12,26 @@ export default function HTML(props) {
                     content="width=device-width, initial-scale=1, shrink-to-fit=no"
                 />
                 {props.headComponents}
-                {/**<script
+                <script
                     dangerouslySetInnerHTML={{
                         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var
         f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-T23N24V');</script>`,
                     }}
-                /> */}
+                />
             </head>
             <body {...props.bodyAttributes}>
                 {props.preBodyComponents}
                 <div key={`body`} id="___gatsby" dangerouslySetInnerHTML={{ __html: props.body }} />
                 {props.postBodyComponents}
-                {/**<noscript
+                <noscript
                     dangerouslySetInnerHTML={{
                         __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T23N24V" height="0" width="0"
         style="display:none;visibility:hidden"></iframe>
         `,
                     }}
-                /> */}
+                />
             </body>
         </html>
     );
