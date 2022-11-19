2b8f83f36606cb69332dad42bc477c0b97a4cc27
Kwinten Fruytier <kwinten@thewave.tech>
2022-11-19 08:53:09 +0100

    trying to fix fonts

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -3,9 +3,6 @@ import { cloneElement } from 'react';
 import { LayoutProvider } from 'layouts/LayoutProvider';
 import { DefaultLayout } from 'layouts/DefaultLayout';
 import type { GatsbyBrowser } from 'gatsby';
-import { Script } from 'gatsby';
-import '@fontsource/cairo';
-import '@fontsource/text-me-one';
 
 export const wrapRootElement: GatsbyBrowser['wrapRootElement'] = ({ element }) => {
     return <LayoutProvider>{element}</LayoutProvider>;
@@ -16,12 +13,5 @@ export const wrapPageElement: GatsbyBrowser['wrapPageElement'] = ({ element, pro
         return cloneElement(element, props);
     }
     const Layout = (element.type as any).Layout ?? DefaultLayout;
-    return (
-        <Layout {...props}>
-            {element}
-            <Script src="https://fonts.googleapis.com/css2?family=Text+Me+One&display=swap" />
-            <Script src="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;500;600;700;800;900;1000&display=swap" />
-            <Script src="https://fonts.googleapis.com/icon?family=Material+Icons" />
-        </Layout>
-    );
+    return <Layout {...props}>{element}</Layout>;
 };
